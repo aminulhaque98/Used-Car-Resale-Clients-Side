450bb2c2909fbf1c0950b53e88440bdec83fb278
aminulhaque98 <aminulhaque419@gmail.com>
2023-01-09 00:05:25 +0530

    fqa and blog component set

--- src/Others/Blog/Blog.js
+++ src/Others/Blog/Blog.js
@@ -5,26 +5,40 @@ const Blog = () => {
         <div>
             <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                    1.What are the different ways to manage a state in a React application?
                 </div>
-                <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-                </div>
-            </div>
-            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-                <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
-                </div>
-                <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                <div className="collapse-content mx-5 px-5">
+                    <p>Answer: The Four Kinds of React State to Manage</p>
+                    <ul class="list-disc">
+                        <li> Local state.</li>
+                        <li>Global state.</li>
+                        <li>Server state.</li>
+                        <li>URL state.</li>
+                    </ul>
                 </div>
             </div>
             <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
-                    Focus me to see content
+                    2.How does prototypical inheritance work?
                 </div>
-                <div className="collapse-content">
-                    <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                <div className="collapse-content mx-5 px-5">
+                    <p>The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the of an object, we use Object.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    3.What is a unit test? Why should we write unit tests?
+                </div>
+                <div className="collapse-content mx-5 px-5">
+                    <p>The main objective of unit testing is to isolate written code to test and determine if it works as intended. Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    4.React vs. Angular vs. Vue?
+                </div>
+                <div className="collapse-content mx-5 px-5">
+                    <p>Vue provides higher customizability and hence is easier to learn than Angular or React. Further, Vue has an overlap with Angular and React with respect to their functionality like the use of components. Hence, the transition to Vue from either of the two is an easy option</p>
                 </div>
             </div>
         </div>
--- src/Others/FQA/FQA.js
+++ src/Others/FQA/FQA.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { FaClock } from 'react-icons/fa';
+
 
 const FQA = () => {
     return (
-        <div>
-            <h1>this is fqa page</h1>
+        <div className='my-20'>
+
+            <div data-theme="valentine" className='text-xl shadow-xl rounded-lg mb-5 p-5'>
+                <h3>What is the tutorial's duration?</h3>
+                <p>
+                    approximately 5-6 month
+                </p>
+            </div>
+
+            <div data-theme="valentine" className='text-xl shadow-xl rounded-lg mb-5 p-5'>
+                <h3>Support time per day</h3>
+                <p>
+                    <p> <FaClock></FaClock> At morning 11 am - 1.00 pm </p>
+                    <p> <FaClock></FaClock> At evening (4-6) pm</p>
+                    <p> <FaClock></FaClock> At night (9-11) pm</p>
+
+
+                </p>
+            </div>
+
+            <div data-theme="valentine" className='text-xl shadow-xl rounded-lg mb-5 p-5'>
+                <h3>Assignment's</h3>
+                <p>
+                    Every student must do 12 assignment ,and per assignment should be average marks 50% .
+                </p>
+            </div>
+
+            <div data-theme="valentine" className='text-xl shadow-xl rounded-lg mb-5 p-5'>
+                <h3>Our team for support</h3>
+                <p>
+                    We have team of 300 members for support
+                </p>
+            </div>
+
         </div>
     );
 };
