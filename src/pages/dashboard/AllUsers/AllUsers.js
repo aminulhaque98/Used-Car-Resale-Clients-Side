f1516c506575b82c0694f330939dc50f12036004
aminulhaque98 <aminulhaque419@gmail.com>
2023-01-11 23:09:01 +0530

    verify admin and admin do  make  any person

--- src/pages/dashboard/AllUsers/AllUsers.js
+++ src/pages/dashboard/AllUsers/AllUsers.js
@@ -1,16 +1,35 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { toast } from 'react-hot-toast';
 
 const AllUsers = () => {
 
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/users');
             const data = await res.json();
             return data;
         }
-    })
+    });
+
+    const handleMakeAdmin = id => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    toast.success('You make him admin successfully');
+                    refetch();
+                }
+                console.log(data)
+            })
+    }
+
 
     return (
         <div>
@@ -37,8 +56,10 @@ const AllUsers = () => {
                                 <td>{user?.name}</td>
                                 <td>{user?.email}</td>
                                 <td>{user?.role}</td>
-                                <td><button className="btn btn-xs btn-active btn-primary">Make Admin</button>
+
+                                <td>{user?.role !== 'Admin' && <button onClick={() => handleMakeAdmin(user?._id)} className="btn btn-xs btn-active btn-primary">Make Admin</button>}
                                 </td>
+
                                 <td><button className="btn btn-xs btn-active btn-secondary">Delete</button></td>
                             </tr>)
                         }
