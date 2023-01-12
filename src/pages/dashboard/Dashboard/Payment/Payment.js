5224e7486ec5bda090498c9c61903b4b077b5e17
aminulhaque98 <aminulhaque419@gmail.com>
2023-01-12 20:03:36 +0530

    payment intent setting

--- src/pages/dashboard/Dashboard/Payment/Payment.js
+++ src/pages/dashboard/Dashboard/Payment/Payment.js
@@ -16,9 +16,11 @@ const Payment = () => {
             <h1 className='text-4xl font-bold text-center my-3'>Payment for your item: <span className='text-secondary'>{itemName}</span></h1>
             <p className='text-xl font-semibold mb-5'>Please pay <strong>${price}</strong> for your buying at your no: {number} and meet location: {meetingLocation}</p>
 
-            <div data-theme="retro" className='mx-auto  sm:w-full md:w-full lg:w-1/2 my-auto rounded-xl p-8'>
+            <div data-theme="synthwave" className='mx-auto  sm:w-full md:w-full lg:w-1/2 my-auto rounded-xl p-8'>
                 <Elements stripe={stripePromise}>
-                    <CheckoutForm />
+                    <CheckoutForm
+                        booking={booking}
+                    />
                 </Elements>
             </div>
 
