a1a9a82803d1da039bcac1e2b1e4746a24e57145
aminulhaque98 <aminulhaque419@gmail.com>
2023-01-14 16:49:32 +0530

    create extra page

---
+++ src/Others/ByUsedCar/ByUesdCar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const ByUesdCar = () => {
+    return (
+        <div className='my-5 bg-indigo-100 '>
+            <div className='p-5'>
+                <h1 className='text-2xl font-bold pl-12 mt-5'>Buying a used car?</h1 >
+                <p className='text-xl pl-12'>here is why you should do it in Car castle</p>
+                <div className='grid gap-6 mt-2 m-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-12'>
+                    <div className="card w-72 bg-white bg-base-100 shadow-xl ">
+                        <figure className="px-10 pt-10">
+                            <img src="https://i.ibb.co/mXHbytX/private.png" alt="private" className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Great Value!</h2>
+                            <p>Skip the dealership margins, and buy directly from individuals</p>
+
+                        </div>
+                    </div>
+                    <div className="card w-72  bg-white bg-base-100 shadow-xl ">
+                        <figure className="px-10 pt-10">
+                            <img src="https://i.ibb.co/tL8Dbc9/car.png" alt="heard" className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Trusted Quality!</h2>
+                            <p>Condition reports upfront & online, down to every scratch</p>
+
+                        </div>
+                    </div>
+                    <div className="card w-72  bg-white bg-base-100 shadow-xl ">
+                        <figure className="px-10 pt-10">
+                            <img src="https://i.ibb.co/j8yL1Zp/rating.png" alt="raiting" className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">All online!</h2>
+                            <p>We handle every step, you just click click click</p>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default ByUesdCar;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../../Banner/Banner';
 import Categories from '../../Categoreis/Categories';
 import Contact from '../../../Others/Contact/Contact';
+import ByUesdCar from '../../../Others/ByUsedCar/ByUesdCar';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Categories></Categories>
+            <ByUesdCar></ByUesdCar>
             <Contact></Contact>
         </div>
     );
