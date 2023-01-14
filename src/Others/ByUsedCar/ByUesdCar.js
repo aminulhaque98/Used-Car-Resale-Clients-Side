f37b849c39c68a53e3e20fc247229dc2f8018a52
aminulhaque98 <aminulhaque419@gmail.com>
2023-01-14 17:33:47 +0530

    modify extra page

--- src/Others/ByUsedCar/ByUesdCar.js
+++ src/Others/ByUsedCar/ByUesdCar.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ByUesdCar = () => {
     return (
-        <div className='my-5 bg-indigo-100 '>
-            <div className='p-5'>
+        <div className='mx-2 my-5 bg-indigo-100 outline-double outline-3 outline-offset-2 rounded-xl'>
+            <div className=''>
                 <h1 className='text-2xl font-bold pl-12 mt-5'>Buying a used car?</h1 >
                 <p className='text-xl pl-12'>here is why you should do it in Car castle</p>
-                <div className='grid gap-6 mt-2 m-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-12'>
+                <div className='grid gap-6 mt-2 mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-12'>
                     <div className="card w-72 bg-white bg-base-100 shadow-xl ">
                         <figure className="px-10 pt-10">
                             <img src="https://i.ibb.co/mXHbytX/private.png" alt="private" className="rounded-xl" />
